790a2eb43547c16cea819cbea704a237011377be
thanhthuybuh2302-png <thanhthuy.buh2302@gmail.com>
2026-09-14 12:00:19 +0700

    Add site configuration

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const siteConfig={
+  productName:'METACARE COLOS LACTOFERRIN PLUS', age:'Dòng 2+ • Dành cho trẻ 2–3 tuổi • Lon 800 g',
+  minigameUrl:null as string|null, purchaseUrl:null as string|null, storeLocatorUrl:null as string|null,
+  comingSoon:true
+}
